273a16e48f232afa7660f3456a62facf1cd0362e
nandhagopal vijayagopal <nangsan21@gmail.com>
2020-10-04 23:37:00 +0530

    feat: added script and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # relaxEyes
+
+A simple script to remind you to take break from staring at your computer. When it's time to take a break your screen will dim it's brightness. Break timings are based on 20-20-20 rule.
+
+### 20-20-20 rule
+
+Your eye doctor may mentioned about it. It's basically for every 20 minutes spent using a screen, you should try to look away at something that is 20 feet away from you for a total of 20 seconds. 
+
+### Download
+
+```shell
+git clone https://github.com/nangsan/relaxEyes.git
+```
+
+### Installation
+
+```shell
+cd relaxEyes
+chmod +x relaxEyes.sh
+mkdir .local/bin
+cp relaxEyes.sh .local/bin
+cd .local/bin
+./relaxEyes.sh -s
+```
+
+### Usage
+
+```shell
+relaxEyes.sh -h
+```
+
---
+++ relaxEyes.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+# relaxEyes.sh
+# Reminds you to relax your eyes.
+
+INSTALL_DIR="$HOME/.local/bin"
+
+# desktop file used for autostart
+DESKTOP_FILE="$HOME/.config/autostart/relaxEyes.desktop"
+
+BREAK_INTERVAL=20 # 20 seconds
+INTERVAL=1200     # 20 minute
+
+INCR=.50    # controls brightness
+
+# set script name
+RELAX_EYES_SH=$(basename "$0")
+
+show_usage() {
+    cat <<-EndUsage
+		Usage: $RELAX_EYES_SH [OPTION]
+		Reminds you to relax your eyes by adjusting monitor brightness.
+		
+		OPTION
+		  -d    Starts the reminder daemon.
+		  -h    Shows this help message.
+		  -r    Removes autostart configuration.
+		  -s    Configures autostart, so that script can start after you login.
+	EndUsage
+    exit 0
+}
+
+start_daemon() {
+    while true; do
+        sleep "$INTERVAL"
+        output=$(xrandr | grep -w connected | awk '{print $1}')   # Get display name!
+        decrease_brightness "$output" "$INCR"
+        sleep "$BREAK_INTERVAL"
+        increase_brightness "$output" "$INCR"
+    done
+}
+
+setup_autostart() {
+
+    # executable file
+    exec_path="relaxEyes.sh"
+
+    # checking PATH variable
+    if [[ ! ":$PATH:" == *":$INSTALL_DIR:"* ]]; then
+        # INSTALL_DIR isn't on PATH
+        # providing absolute path
+        exec_path="$INSTALL_DIR/relaxEyes.sh"
+    fi
+
+    cat >"$DESKTOP_FILE" <<-EndFile
+		[Desktop Entry]
+		Type=Application
+		Name=Relax Eyes
+		Comment=Reminds you to take break for your eyes
+		Exec=$exec_path -d
+		Terminal=false
+		StartupNotify=false
+	EndFile
+    echo
+    echo "Autostart configuration done and you'll be reminded to take a break."
+    echo "Look out the window and enjoy the break!"
+    echo "Now save you work then logout and login again!"
+    exit 0
+}
+
+remove_autostart() {
+    if [ -f "$DESKTOP_FILE" ]; then
+        rm "$DESKTOP_FILE"
+    fi
+    echo
+    echo "Removed autostart configuration and you won't be reminded."
+    echo "Now save your work then logout and login again!"
+    exit 0
+}
+
+decrease_brightness() {
+    old_brightness=$(xrandr --verbose | grep rightness | awk '{ print $2 }')
+    bright=$(echo "scale=2; $old_brightness - $2" | bc)
+    xrandr --output "$1" --brightness "$bright"
+}
+
+increase_brightness() {
+    old_brightness=$(xrandr --verbose | grep rightness | awk '{ print $2 }')
+    bright=$(echo "scale=2; $old_brightness + $2" | bc)
+    xrandr --output "$1" --brightness "$bright"
+}
+
+if [[ -z "$1" || "$1" = "-h" || "$1" = "--help" ]]; then
+    show_usage
+elif [ "$1" = "-d" ]; then
+    start_daemon
+elif [ "$1" = "-s" ]; then
+    setup_autostart
+elif [ "$1" = "-r" ]; then
+    remove_autostart
+else
+    show_usage
+fi
+
+exit 0
